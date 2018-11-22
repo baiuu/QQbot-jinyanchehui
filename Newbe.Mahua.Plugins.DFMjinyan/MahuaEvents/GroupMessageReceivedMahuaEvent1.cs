dac6d1593ef4694907e5606d3b165403e83136cb
baiuu <767471286@qq.com>
2018-11-22 14:42:06 +0900

    1.0.0

---
+++ Newbe.Mahua.Plugins.DFMjinyan/MahuaEvents/GroupMessageReceivedMahuaEvent1.cs
@@ -0,0 +1,55 @@
+﻿using Newbe.Mahua.MahuaEvents;
+using System;
+
+namespace Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents
+{
+    public static class Common
+    {
+        //存储全局静态变量
+        public static string msg = "000";  //消息缓存
+        public static string msgQq = "000000000";  //缓存消息的QQ号
+        public static System.Collections.Generic.List<string> QQList = new System.Collections.Generic.List<string>();   //禁言管理QQ列表
+        public static System.Collections.Generic.List<string> DieList = new System.Collections.Generic.List<string>();   //禁言关键词
+        public static int execuateTime = 1;   //禁言时间
+    }
+    /// <summary>
+    /// 群消息接收事件
+    /// </summary>
+    /// 
+    public class GroupMessageReceivedMahuaEvent1
+        : IGroupMessageReceivedMahuaEvent
+    {
+        private readonly IMahuaApi _mahuaApi;
+
+        public GroupMessageReceivedMahuaEvent1(
+            IMahuaApi mahuaApi)
+        {
+            _mahuaApi = mahuaApi;
+        }
+
+        public void ProcessGroupMessage(GroupMessageReceivedContext context)
+        {
+            var token = context.MessageCancelToken;
+            Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.msg = context.Message;
+            Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.msgQq = context.FromQq;
+            for (int k = 0; k < Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.DieList.Count; k++)
+            {
+                if (Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.msg.Contains(Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.DieList[k]))
+                {
+                    for (int l = 0; l < Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.QQList.Count; l++)
+                    {
+                        if (Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.msgQq == Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.QQList[l])
+                        {
+                            _mahuaApi.BanGroupMember(context.FromGroup, Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.QQList[l], TimeSpan.FromMinutes(Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.execuateTime));
+                            token.Cancel();
+                        }
+                    }
+                }
+            }
+            // todo 填充处理逻辑
+           // throw new NotImplementedException();
+
+            // 不要忘记在MahuaModule中注册
+        }
+    }
+}
---
+++ Newbe.Mahua.Plugins.DFMjinyan/MahuaEvents/PrivateMessageFromFriendReceivedMahuaEvent1.cs
@@ -0,0 +1,297 @@
+﻿using Newbe.Mahua.MahuaEvents;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents
+{
+    /// <summary>
+    /// 来自好友的私聊消息接收事件
+    /// </summary>
+    public class PrivateMessageFromFriendReceivedMahuaEvent1
+        : IPrivateMessageFromFriendReceivedMahuaEvent
+    {
+        private readonly IMahuaApi _mahuaApi;
+
+        //getDigit()函数从字符串中截取出数字并转换成整型
+        public int getDigit(string str)
+        {
+            Regex r = new Regex("\\d+\\.?\\d*");    //设置匹配用的正则表达式
+            bool isMatch = r.IsMatch(str);
+            MatchCollection mc = r.Matches(str);
+            string result = string.Empty;
+
+            //生成处理后的字符串，应该只有数字部分
+            for (int i = 0; i < mc.Count; i++)
+            {
+                result += mc[i];
+            }
+
+            return int.Parse(result);   //数字字符串转换为整型数
+        }
+        public string getDigit1(string str)
+        {
+            string result = str.Substring(str.LastIndexOf("=") + 1, (str.Length - str.LastIndexOf("=") - 1));
+            return result;   //字符串保存
+        }
+
+        public PrivateMessageFromFriendReceivedMahuaEvent1(
+            IMahuaApi mahuaApi)
+        {
+            _mahuaApi = mahuaApi;
+        }
+
+        public void ProcessFriendMessage(PrivateMessageFromFriendReceivedContext context)
+        {
+            // todo 填充处理逻辑
+            //throw new NotImplementedException();
+            //功能介绍
+            _mahuaApi.SendPrivateMessage(context.FromQq)
+                .Text("通过和机器人好友聊天，可以设置机器人的运行模式，禁言词条以及禁言QQ列表")
+                .Newline()
+                .Text("使用以下命令，可以更改相应的运行特性")
+                .Newline()
+                .Text("添加QQ号到禁言列表 addQQ=xxxxxxxx X是QQ号")
+                .Newline()
+                .Text("从QQ号列表删除QQ号 delQQ=xxxxxxxx X是QQ号")
+                .Newline()
+                .Text("删除所有QQ号 clearQQ")
+                .Newline()
+                .Text("查看所有Q0Q号 showQQ")
+                .Newline()
+                .Text("设置禁言时间 banTime=X X代表禁言时间，单位分钟")
+                .Newline()
+                .Text("添加禁言词条 addLine=X X代表禁言词条")
+                .Newline()
+                .Text("删除禁言词条 delLine=X X代表禁言词条")
+                .Newline()
+                .Text("删除所有禁言词条 clearLine")
+                .Newline()
+                .Text("查看所有禁言词条 showLine")
+                .Done();
+
+            //匹配命令类型的正则表达式
+            Regex addQQ = new Regex("addQQ");
+            Regex ban = new Regex("banTime");
+            Regex delQQ = new Regex("delQQ");
+            Regex addLine = new Regex("addLine");
+            Regex delLine = new Regex("delLine");
+            Regex clearLine = new Regex("clearLine");
+            Regex clearQQ = new Regex("clearQQ");
+            Regex showQQ = new Regex("showQQ");
+            Regex showLine = new Regex("showLine");
+
+            //根据不同的输入命令调整相应的参数值
+            if (addQQ.IsMatch(context.Message) == true)
+            {
+                _mahuaApi.SendPrivateMessage(context.FromQq)
+                    .Text("进入添加QQ模式 ")
+                    .Done();
+                string result = getDigit1(context.Message);
+                int kongzhi = 0;
+                for (int k = 0; k < Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.QQList.Count; k++)
+                {
+                    if (result != Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.QQList[k])
+                    {
+                        kongzhi = 1;
+                    }
+                    else
+                    {
+                        kongzhi = 2;
+                    }
+                }
+                if (kongzhi == 1 || kongzhi==0)
+                {
+                    Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.QQList.Add(result);
+                    _mahuaApi.SendPrivateMessage(context.FromQq)
+                        .Text("该QQ已经添加进列表 ")
+                        .Text(Convert.ToString(result))
+                        .Done();
+                }
+                if(kongzhi==2)
+                {
+                    _mahuaApi.SendPrivateMessage(context.FromQq)
+                        .Text("QQ号列表中已存在 ")
+                        .Text(Convert.ToString(result))
+                        .Done();
+
+                }
+            }
+
+            if (ban.IsMatch(context.Message) == true)
+            {
+                int result = getDigit(context.Message);
+                Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.execuateTime = result;
+                _mahuaApi.SendPrivateMessage(context.FromQq)
+                    .Text("禁言时间已调整为 ")
+                    .Text(Convert.ToString(result))
+                    .Done();
+            }
+
+            if (delQQ.IsMatch(context.Message) == true)
+            {
+                int test = 0;
+                string result = getDigit1(context.Message);
+                for (int i = Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.QQList.Count - 1; i >= 0; i--)
+                {
+                    if (Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.QQList[i] == result)
+                    {
+                        Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.QQList.Remove(Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.QQList[i]);
+                        test = 1;
+                    }
+                    else
+                    {
+                        test = 2;
+                    }
+                }
+                if (test==1)
+                {
+                    _mahuaApi.SendPrivateMessage(context.FromQq)
+                        .Text("该QQ已从列表删除 ")
+                        .Text(Convert.ToString(result))
+                        .Done();
+                }
+                if(test==2||test==0)
+                {
+                    _mahuaApi.SendPrivateMessage(context.FromQq)
+                        .Text("QQ号列表中已存在 ")
+                        .Text(Convert.ToString(result))
+                        .Done();
+                }
+            }
+
+            if (addLine.IsMatch(context.Message) == true)
+            {
+                _mahuaApi.SendPrivateMessage(context.FromQq)
+                    .Text("进入添加词条模式 ")
+                    .Done();
+                string result = getDigit1(context.Message);
+                int kongzhi = 0;
+                for (int k = 0; k < Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.DieList.Count; k++)
+                {
+                    if (result != Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.DieList[k])
+                    {
+                        kongzhi = 1;
+                    }
+                    else
+                    {
+                        kongzhi = 2;
+                    }
+                }
+                if (kongzhi == 1 || kongzhi == 0)
+                {
+                    Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.DieList.Add(result);
+                    _mahuaApi.SendPrivateMessage(context.FromQq)
+                        .Text("该词条已经添加进列表 ")
+                        .Text(Convert.ToString(result))
+                        .Done();
+                }
+                if (kongzhi==2)
+                {
+                    _mahuaApi.SendPrivateMessage(context.FromQq)
+                       .Text("词条列表中不存在 ")
+                       .Text(Convert.ToString(result))
+                       .Done();
+                }
+            }
+
+            if (delLine.IsMatch(context.Message) == true)
+            {
+                int test = 0;
+                string result = getDigit1(context.Message);
+                for (int i = Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.DieList.Count - 1; i >= 0; i--)
+                {
+                    if (Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.DieList[i] == result)
+                    {
+                        Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.DieList.Remove(Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.DieList[i]);
+                        test = 1;
+                    }
+                    else
+                    {
+                        test = 2;
+                    }
+                }
+                if (test == 1)
+                {
+                    _mahuaApi.SendPrivateMessage(context.FromQq)
+                        .Text("该词条已从列表删除 ")
+                        .Text(Convert.ToString(result))
+                        .Done();
+                }
+                if (test == 2 || test == 0)
+                {
+                    _mahuaApi.SendPrivateMessage(context.FromQq)
+                        .Text("词条列表中不存在 ")
+                        .Text(Convert.ToString(result))
+                        .Done();
+                }
+            }
+
+            if (clearLine.IsMatch(context.Message) == true)
+            {
+                Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.DieList.Clear();
+                _mahuaApi.SendPrivateMessage(context.FromQq)
+                    .Text("词条列表已清空 ")
+                    .Done();
+            }
+            if (clearQQ.IsMatch(context.Message) == true)
+            {
+                Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.QQList.Clear();
+                _mahuaApi.SendPrivateMessage(context.FromQq)
+                    .Text("QQ号列表已清空 ")
+                    .Done();
+            }
+
+            if (showQQ.IsMatch(context.Message) == true)
+            {
+                string list = "";
+                int kongzhi = 0;
+                for (int k = 0; k < Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.QQList.Count; k++)
+                {
+                    list = list + Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.QQList[k] + ",";
+                    kongzhi = 1;
+                }
+                if (kongzhi == 1)
+                {
+                    _mahuaApi.SendPrivateMessage(context.FromQq)
+                        .Text("QQ号列表：")
+                        .Newline()
+                        .Text(list)
+                        .Done();
+                }
+                if (kongzhi == 0)
+                {
+                    _mahuaApi.SendPrivateMessage(context.FromQq)
+                        .Text("QQ号列表为空")
+                        .Done();
+                }
+            }
+
+            if (showLine.IsMatch(context.Message) == true)
+            {
+                string list="";
+                int kongzhi = 0;
+                for (int k = 0; k < Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.DieList.Count; k++)
+                {
+                    list = list+ Newbe.Mahua.Plugins.DFMjinyan.MahuaEvents.Common.DieList[k] + ",";
+                    kongzhi = 1;
+                }
+                if (kongzhi==1)
+                {
+                    _mahuaApi.SendPrivateMessage(context.FromQq)
+                        .Text("禁言词条列表：")
+                        .Newline()
+                        .Text(list)
+                        .Done();
+                }
+                if (kongzhi == 0)
+                {
+                    _mahuaApi.SendPrivateMessage(context.FromQq)
+                        .Text("禁言词条列表为空")
+                        .Done();
+                }
+            }
+
+                // 不要忘记在MahuaModule中注册
+            }
+    }
+}
